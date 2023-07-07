efd63d2248c62c185fc6c646ac190c9e84b287c0
stefan <superstefo@gmail.com>
2023-07-07 15:29:11 +0300

    add home pic, remove theory page

--- componentz/TextComponents.js
+++ componentz/TextComponents.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export const Heading1 = ({ children, id }) => {
   return (
     <>
-    <p></p>
+      <p></p>
       <br></br>
       <h1 id={id} className="mb-4 text-1xl font-extrabold leading-none tracking-tight text-gray-900 md:text-1xl lg:text-1xl dark:text-white">{children}</h1>
     </>
@@ -14,6 +14,14 @@ export const Link1 = ({ href, text }) => {
   return (
     <>
       <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href={href}> {text} </Link>
+    </>
+  );
+}
+
+export const Red = ({ children, id }) => {
+  return (
+    <>
+      <span id={id} className="font-medium text-red-700">{children}</span>
     </>
   );
 }
